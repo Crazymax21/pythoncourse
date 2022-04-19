0560471daf8836bf4ec5cbcffed381d2760fcdde
Crazymax21 <bpgachevm@gmail.com>
2022-04-19 17:22:08 +1000

    Четыре задания

---
+++ Lesson 1/Task_2.py
@@ -0,0 +1,11 @@
+#2. Пользователь вводит время в секундах.
+#Переведите время в часы, минуты, секунды и выведите в формате чч:мм:сс.
+# Используйте форматирование строк.
+
+user_time_seconds = int(input('Введите время в секундах: '))
+
+hours = user_time_seconds//60//60
+minutes = user_time_seconds//60-(hours*60)
+seconds = user_time_seconds-((hours*60+minutes)*60)
+
+print(f'Время в формате чч:мм:сс {hours}:{minutes}:{seconds}')
---
+++ Lesson 1/Task_4.py
@@ -0,0 +1,13 @@
+# 4. Пользователь вводит целое положительное число. Найдите самую большую цифру в числе.
+# Для решения используйте цикл while и арифметические операции.
+
+user_number = int(input('Введите число: '))
+
+user_max = user_number%10
+
+while user_number != 0:
+    if user_max <= user_number%10:
+        user_max = user_number%10
+    user_number = user_number // 10
+
+print(f'Самая большая цифра: {user_max}')
