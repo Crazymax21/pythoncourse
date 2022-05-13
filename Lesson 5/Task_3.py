aa975e5087a3da64a84be5888f78de8d79346613
Crazymax21 <bpgachevm@gmail.com>
2022-05-13 10:53:34 +1000

    Задание 3 урока №5 выполенно.

--- Lesson 5/Task_3.py
+++ Lesson 5/Task_3.py
@@ -6,3 +6,18 @@
 # Иванов 23543.12
 # Петров 13749.32
 
+average_sum = 0
+staff_count = 0
+
+with open('task_3.txt', 'r', encoding='utf-8') as task3_file:
+    print(f'Сотрудники с зарплатой менее 20000:')
+    for person_data in task3_file.read().split('\n'):
+        person_data_list = person_data.split(' ')
+        if int(person_data_list[1]) < 20000:
+            print(f'{person_data_list}')
+        staff_count += 1
+        average_sum += int(person_data_list[1])
+    print('Средняя зарплата сотрудников: ', average_sum/staff_count)
+
+
+
